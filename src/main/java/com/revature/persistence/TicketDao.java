db90a1b68a400ceccce455223e1673ec83997472
Braydensn <braydensn@gmail.com>
2022-12-09 16:54:38 -0600

    work on ticket pojo and dao

--- src/main/java/com/revature/persistence/TicketDao.java
+++ src/main/java/com/revature/persistence/TicketDao.java
@@ -1,4 +1,40 @@
 package com.revature.persistence;
 
+import com.revature.pojo.Ticket;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class TicketDao {
+    Connection connection;
+
+    public TicketDao(){
+        connection = ConnectionManager.getConnection();
+    }
+
+    //CRUD
+    //Create a new ticket
+    //Read existing tickets, filter by status, sort by date submitted/processed?
+    //Update a ticket
+    //Delete a ticket
+
+    public void create(Ticket ticket){
+        try {
+            String sql = "INSERT INTO tickets (amount, description, status, date_submitted, date_processed, user_id)" +
+                    "VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            pstmt.setDouble(1, ticket.getAmount());
+            pstmt.setString(1, ticket.getDescription());
+            pstmt.setString(1, ticket.getStatus());
+
+            //Figure out what to do with these dates, might just use strings instead
+            //pstmt.setDate(1, ticket.getDateSubmitted());
+            //pstmt.setDate(1, ticket.getDateProcessed());
+            pstmt.setInt(1, ticket.getUserId());
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/com/revature/pojo/Ticket.java
+++ src/main/java/com/revature/pojo/Ticket.java
@@ -9,7 +9,90 @@ public class Ticket {
     private String status;
     private LocalDateTime dateSubmitted;
     private LocalDateTime dateProcessed;
-    private Integer employeeId;
+    private Integer userId;
 
+    public Ticket() {}
 
+    public Ticket(Integer ticketId, Double amount, String description, String status, LocalDateTime dateSubmitted, LocalDateTime dateProcessed, Integer userId) {
+        this.ticketId = ticketId;
+        this.amount = amount;
+        this.description = description;
+        this.status = status;
+        this.dateSubmitted = dateSubmitted;
+        this.dateProcessed = dateProcessed;
+        this.userId = userId;
+    }
+
+    public Ticket(Double amount, String description, String status, LocalDateTime dateSubmitted, LocalDateTime dateProcessed, Integer userId) {
+        this.amount = amount;
+        this.description = description;
+        this.status = status;
+        this.dateSubmitted = dateSubmitted;
+        this.dateProcessed = dateProcessed;
+        this.userId = userId;
+    }
+
+    public Ticket(Double amount, String description, String status, LocalDateTime dateSubmitted, LocalDateTime dateProcessed) {
+        this.amount = amount;
+        this.description = description;
+        this.status = status;
+        this.dateSubmitted = dateSubmitted;
+        this.dateProcessed = dateProcessed;
+    }
+
+    public Integer getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Integer ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDateSubmitted() {
+        return dateSubmitted;
+    }
+
+    public void setDateSubmitted(LocalDateTime dateSubmitted) {
+        this.dateSubmitted = dateSubmitted;
+    }
+
+    public LocalDateTime getDateProcessed() {
+        return dateProcessed;
+    }
+
+    public void setDateProcessed(LocalDateTime dateProcessed) {
+        this.dateProcessed = dateProcessed;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
 }
